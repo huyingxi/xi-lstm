36bcb4834015fa99fc31db38f53b665941c23cf8
Huan LI <zixia@zixia.net>
2017-12-16 03:18:16 +0800

    clean script

--- scripts/download.sh
+++ scripts/download.sh
@@ -7,14 +7,18 @@ if [ -d train_test ]
 then
     echo 'train_test already exist';
 else
-    curl -LO https://github.com/zixia/xi-lstm/releases/download/v0.0.1/train_test.zip && unzip train_test.zip && rm train_test.zip
+    curl -LO https://github.com/zixia/xi-lstm/releases/download/v0.0.1/train_test.zip
+    unzip train_test.zip
+    rm train_test.zip
 fi
 
 if [ -d word2vec ]
 then
     echo 'word2vec already exist'
 else
-    curl -LO https://github.com/zixia/xi-lstm/releases/download/v0.0.1/word2vec.zip && unzip word2vec.zip && rm word2vec.zip
+    curl -LO https://github.com/zixia/xi-lstm/releases/download/v0.0.1/word2vec.zip
+    unzip word2vec.zip
+    rm word2vec.zip
 fi
 
 if [ -f word2vec_google300_for_NYT.pkl ]
